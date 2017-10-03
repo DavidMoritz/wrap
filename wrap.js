45fd4386c4d93031138edccf8b3a5dbbf071839c
Moritz, David <Moritz.David@principal.com>
2017-10-03 15:38:40 -0500

    get rid of jquery

--- wrap.js
+++ wrap.js
@@ -1,9 +1,12 @@
 class Wrap {
 	constructor() {
-		this.height = Number($("[name=height]").val());
-		this.length = Number($("[name=length]").val());
-		this.depth = Number($("[name=depth]").val());
-		this.roll = Number($("[name=roll-length]:checked").val());
+		const getNumber = (query, check) => Number(document.querySelector(`[name=${query}]${check ? ":checked" : ""}`).value);
+
+		this.height = getNumber("height");
+		this.length = getNumber("length");
+		this.depth = getNumber("depth");
+		this.roll = getNumber("roll-length", true);
+		this.buffer = 1;
 	}
 	get longest() {
 		return Math.max(this.height, this.length, this.depth);
@@ -11,23 +14,23 @@ class Wrap {
 	get shortest() {
 		return Math.min(this.height, this.length, this.depth);
 	}
+	get waist() {
+		return 2 * (this.height + this.length + this.depth - this.longest);
+	}
 	get cape() {
 		return this.longest + this.shortest;
 	}
 	get amount() {
-		const tempX = Math.max(this.cape, this.waist) + 1;
-		const tempN = Math.min(this.cape, this.waist) + 1;
+		const capeLength = Math.max(this.cape, this.waist) + this.buffer;
+		const waistLength = Math.min(this.cape, this.waist) + this.buffer;
 
-		if(tempX <= this.roll) {
-			return tempN;
-		} else if (tempN <= this.roll) {
-			return tempX;
+		if(capeLength <= this.roll) {
+			return waistLength;
+		} else if (waistLength <= this.roll) {
+			return capeLength;
 		} else {
 			return "too large for this roll";
 		}
-	}
-	get waist() {
-		return 2 * (this.height + this.length + this.depth - this.longest) + 1;
 	}
 }
 
@@ -35,31 +38,46 @@ function calculateTotal() {
 	const wrap = new Wrap();
 	const foot = wrap.amount > 23 ? "feet" : "foot";
 	const inch = wrap.amount % 12 === 1 ? "inch" : "inches";
-	let result = `${wrap.amount} inches`;
+	let result = `<span class="unit">${wrap.amount}</span>`;
 
 	if(wrap.amount > 12) {
-		result += ` <span class="feet">(${parseInt(wrap.amount/12)} ${foot} - ${wrap.amount%12} ${inch})</span>`;
+		result += ` <span class="parse">(${parseInt(wrap.amount/12)} ${foot} - ${wrap.amount%12} ${inch})</span>`;
 	}
 
-	$("#demo").html(result);
+	document.querySelector("#result").innerHTML = result;
+}
+
+function switchUnits() {
+	if(document.body.className === "cm-unit") {
+		document.body.className = "";
+	} else {
+		document.body.className = "cm-unit";
+	}
 }
 
 function clickButton(e) {
-	const $target = $(e.target);
-	const $input = $target.closest(".input-group").find("input");
-	let val = Number($input.val());
+	const attr = e.currentTarget.getAttribute("for");
+	const targetClass = e.target.className;
+	const input = document.querySelector(`[name=${attr}]`);
+	let val = Number(input.value);
 
-	if($target.hasClass("plus")) {
-		val += $target.hasClass("double") ? 5 : 1;
-	} else {
-		val -= $target.hasClass("double") ? 5 : 1;
+	if(targetClass.indexOf("plus") >= 0) {
+		val += targetClass.indexOf("double") >= 0 ? 5 : 1;
+	} else if(targetClass.indexOf("minus") >= 0) {
+		val -= targetClass.indexOf("double") >= 0 ? 5 : 1;
 	}
 
-	$input.val(Math.max(0, val));
+	input.value = Math.max(0, val);
 	calculateTotal();
 }
 
-$(() => {
-	$("input").on("input change", calculateTotal);
-	$("button").on("click", clickButton);
-});
+function on(event, selector, func) {
+	document.querySelectorAll(selector).forEach(el => {
+		el.addEventListener(event, func);
+	});
+}
+
+on("input", "input", calculateTotal);
+on("change", "input", calculateTotal);
+on("click", ".input-group", clickButton);
+on("click", ".title", switchUnits);
